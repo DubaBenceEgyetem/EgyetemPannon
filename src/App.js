ee7fb18d2fa61f4058c0deb1a11bc94e86625276
Duba Bence <duba.bence22@gmail.com>
2023-06-16 16:43:36 +0200

    updatedd

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
 import LoginForm from '../src/LoginForm/LoginForm';
 import '../src/LoginForm/LoginForm.css';
 
 
 function App() {
   
+  const [showForm, setShowForm] = useState(false);
+
+  useEffect(() => {
+    setShowForm(true); // Amikor a komponens betöltődik, beállítjuk a showForm értékét true-ra, hogy elinduljon az animáció
+ 
+}, []);
 
   return (
     
-      <div> 
-      <LoginForm/>
-      </div>  
+      <div className={`Wallpaper ${showForm ? 'show' : ''}`}> 
+        <div className={`BackgroundOverlay ${showForm ? 'show' : ''}`}>
+        <LoginForm/>
+        </div>
+        </div>
    
   );
 }
