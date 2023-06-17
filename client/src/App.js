696bb3710a5dbc5da4044d346e9e91910da34a7a
Duba Bence <duba.bence22@gmail.com>
2023-06-17 20:59:38 +0200

    updated

--- client/src/App.js
+++ client/src/App.js
@@ -8,32 +8,63 @@ import Navbar from './Navbar/Navbar';
 
 
 
+
+
 function App() {
   
+  const [backendData, setBackendData] = useState([{}]);
   const [showForm, setShowForm] = useState(false);
 
+  
+  
   useEffect(() => {
     setShowForm(true); // Amikor a komponens betöltődik, beállítjuk a showForm értékét true-ra, hogy elinduljon az animáció
- 
-}, []);
+    
+  }, [])
+  
+  
+  const [currentForm, setCurrentForm] = useState('login');
+  
+  const toggleForm = (formName) => {
+    setCurrentForm(formName);
+  }
+  
 
-
-const [currentForm, setCurrentForm] = useState('login');
-
-const toggleForm = (formName) => {
-  setCurrentForm(formName);
-}
-
+  //kap valamit ay apitol
+  useEffect(() =>
+    {
+      fetch("/api").then(
+        response => response.json()
+       ).then(
+        data => {
+          setBackendData(data)
+        }
+       )
+    }, [])
+    //vege
 
   return (
     
     <div className={`Wallpaper ${showForm ? 'show' : ''}`}> 
-      <div className={`BackgroundOverlay ${showForm ? 'show' : ''}`}>
+     <div className={`BackgroundOverlay ${showForm ? 'show' : ''}`}>
         <Navbar/>
+          {
+              (typeof backendData.users ==='undefined')
+               ? 
+               (
+                <p>loading..</p>
+               )
+               :
+               (
+                backendData.users.map((user, i) => 
+                <p key={i}>{user}</p>
+                )
+               )
+          }
         <div>
           {
             currentForm === "login" ? <LoginForm onFormSwitch={toggleForm}/> : <RegisterForm onFormSwitch={toggleForm}/>
-          }
+          }    
         </div>
       </div> 
     </div>
